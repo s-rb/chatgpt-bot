3b2ac6444274acbd089ecd5c9d04fb6eba430f85
Roman Surkov <s.rb@list.ru>
2023-02-28 20:15:44 +0300

    Tested and works fine

--- src/main/java/ru/list/surkovr/chatgptbot/ChatGPTBot.java
+++ src/main/java/ru/list/surkovr/chatgptbot/ChatGPTBot.java
@@ -4,7 +4,10 @@ import com.theokanning.openai.completion.CompletionRequest;
 import com.theokanning.openai.service.OpenAiService;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.DeleteMessage;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
 import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.User;
@@ -15,6 +18,7 @@ import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.Serializable;
 import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
@@ -123,7 +127,8 @@ public class ChatGPTBot extends TelegramLongPollingBot {
             String generatedText = response.getChoices().get(0).getText();
             log.info(format("Sending response to user with ID %s in chat %s : %s", userId, chatIdString, generatedText));
 
-            sendMessageToUser(chatId, generatedText, null);
+            sendMessageToUser(chatId, generatedText != null && !generatedText.isBlank()
+                    ? generatedText : "Sorry, I don't have answer", null);
         } else {
             sendMessageToUser(userId, "Access denied", null);
             askForAccessApproval(from);
@@ -175,40 +180,58 @@ public class ChatGPTBot extends TelegramLongPollingBot {
         switch (data[0]) {
             case "accessApproval" -> {
                 ru.list.surkovr.chatgptbot.User user = userService.updateStatus(tgUser, chatId, UserStatus.PENDING);
+                editMessage("Access request sent", chatId, callbackQuery.getMessage().getMessageId());
                 askAdminForAccessConfirmation(user);
             }
             case "accessDecline" -> {
                 ru.list.surkovr.chatgptbot.User user = userService.updateStatus(tgUser, chatId, UserStatus.PENDING);
+                deleteMessage(chatId, callbackQuery.getMessage().getMessageId());
                 sendMessageToUser(user.getChatId(), "Access denied", null);
             }
             case "accessApproveConfirmation" -> {
                 long approvedUserId = Long.parseLong(data[1]);
                 ru.list.surkovr.chatgptbot.User user = userService.updateStatus(approvedUserId, UserStatus.APPROVED);
-                sendMessageToUser(user.getChatId(), "Access granted. Welcome!", null);
-                sendMessageToAdmin("You have approved access for user " + user.getFirstName() + " " +
-                        user.getLastName() + " (" + user.getUsername() + ")", null);
+                sendMessageToUser(user.getChatId(), "Access granted. Welcome! Now you can ask your questions", null);
+                editMessage("You have approved access for user " + user.getFirstName() + " " +
+                        user.getLastName() + " (" + user.getUsername() + ")", adminChatId, callbackQuery.getMessage().getMessageId());
             }
             case "accessDeclineConfirmation" -> {
                 long declinedUserId = Long.parseLong(data[1]);
                 ru.list.surkovr.chatgptbot.User user = userService.updateStatus(declinedUserId, UserStatus.DECLINED);
                 sendMessageToUser(user.getChatId(), "Your request for the access was declined", null);
-                sendMessageToAdmin("You have declined access request from user " + user.getFirstName() + " " +
-                        user.getLastName() + " (" + user.getUsername() + ")", null);
+                editMessage("You have declined access request from user " + user.getFirstName() + " " +
+                        user.getLastName() + " (" + user.getUsername() + ")", adminChatId, callbackQuery.getMessage().getMessageId());
             }
             default -> {
                 String errMsg = format("Got unknown, unprocessable command: %s, chatId: %s, userId: %s",
                         Arrays.toString(data), chatId, tgUser.getId());
-                log.warning(errMsg);
-                sendMessageToAdmin(errMsg, null);
+                sendErrorToAdmin(errMsg, null);
             }
         }
+    }
+
+    private void editMessage(String newText, Long chatId, Integer messageId) {
+        final EditMessageText msg = new EditMessageText();
+        msg.setChatId(chatId);
+        msg.setMessageId(messageId);
+        msg.setText(newText);
+        msg.setReplyMarkup(null);
+        executeMsgAction(msg);
+    }
+
+    private void deleteMessage(Long chatId, Integer messageId) {
+        executeMsgAction(new DeleteMessage(String.valueOf(chatId), messageId));
     }
 
     private void sendMessageToUser(long chatId, String message, InlineKeyboardMarkup inlineKeyboardMarkup) {
         SendMessage sendMessage = new SendMessage(String.valueOf(chatId), message);
         if (inlineKeyboardMarkup != null) sendMessage.setReplyMarkup(inlineKeyboardMarkup);
+        executeMsgAction(sendMessage);
+    }
+
+    private <T extends Serializable, Method extends BotApiMethod<T>> void executeMsgAction(Method method) {
         try {
-            execute(sendMessage);
+            execute(method);
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
@@ -216,6 +239,11 @@ public class ChatGPTBot extends TelegramLongPollingBot {
 
     private void sendMessageToAdmin(String message, InlineKeyboardMarkup inlineKeyboardMarkup) {
         sendMessageToUser(adminChatId, message, inlineKeyboardMarkup);
+    }
+
+    private void sendErrorToAdmin(String message, InlineKeyboardMarkup inlineKeyboardMarkup) {
+        log.warning(message);
+        sendMessageToUser(adminChatId, "#ERROR\n\n" + message, inlineKeyboardMarkup);
     }
 }
 
